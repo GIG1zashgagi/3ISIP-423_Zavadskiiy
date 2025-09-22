f5be67da799620204e96b001674c083ce3483574
Илья Завадский <ilazavadskij715@gmail.com>
2025-09-22 10:02:24 +0300

    Создаем класс для хранения данных о расходе. Этот класс будет хранить название покупки и сумму

--- 3ISIP-423_Zavadskiy/Program.cs
+++ 3ISIP-423_Zavadskiy/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Expense
+{
+    public string Name { get; set; }    // Название товара или услуги
+    public decimal Amount { get; set; } // Сумма в рублях
+
+    // Метод для красивого вывода информации о расходе
+    public override string ToString() => $"{Name}; {Amount} руб.";
+}
